4bc373e6a670c7d564a39f8b60e8d044405bcbdc
Marty <maruri2424@gmail.com>
2022-05-26 22:00:55 -0700

    add display for weather data and api to get pic based on weather description

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ async function processWeather(city) {
   try {
     const data = await getData(city);
     const processed = filterWeather(data);
+    displayWeather(processed);
     console.log(processed);
   } catch (err) {
     console.error(err);
@@ -44,4 +45,42 @@ function getWeather() {
   } else {
     input.reportValidity();
   }
+}
+
+function displayWeather(obj) {
+  clear();
+  const content = document.getElementById('content');
+  const name = document.getElementById('name');
+  name.textContent = obj.name;
+  const weather = obj.weather.main;
+  const pWeather = document.createElement('p');
+  pWeather.textContent = `Weather: ${weather}`;
+  const { temp } = obj.main;
+  const pTemp = document.createElement('p');
+  pTemp.textContent = `Temp: ${temp} F`;
+  content.appendChild(pWeather);
+  content.appendChild(pTemp);
+  setPic(obj);
+}
+
+async function setPic(obj) {
+  const q = obj.weather.main;
+  const imgUrl = await getPic(q);
+  const img = document.createElement('img');
+  img.src = imgUrl;
+  const content = document.getElementById('content');
+  content.appendChild(img);
+}
+
+async function getPic(q) {
+  const url = `https://pixabay.com/api/?key=27682534-5d9a93393ee400253db992ca2&q=${q}+weather`;
+  const response = await fetch(url, { mode: 'cors' });
+  const json = await response.json();
+  const imageUrl = json.hits[0].largeImageURL;
+  return imageUrl;
+}
+
+function clear() {
+  const content = document.getElementById('content');
+  content.innerHTML = '';
 }
